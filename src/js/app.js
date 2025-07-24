b936d7a3c07e4c3207e947fc497bee4d927adedd
Britney Escoffery <britneybescoffery@gmail.com>
2025-07-24 00:43:11 +0000

    edited dropdowns

--- src/js/app.js
+++ src/js/app.js
@@ -19,11 +19,11 @@ import "../style/index.css";
         lastName: null,
         role: null,
         country: null,
-        city: null
+        city:null
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
+  console.log("These are the current variables: ", variables.name); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
@@ -33,9 +33,13 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name == null ? "" : variables.name} ${
+    variables.lastName == null ? "" : variables.lastName
+  }</h1>
+          <h2>${variables.role == null ? "" : variables.role}</h2>
+          <h3>${variables.city == null ? "" : variables.city}, ${
+    variables.country == null ? "" : variables.country
+  }</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
